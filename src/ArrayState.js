3cb923d6b35437f00d6f924915826282dd298cf5
Haneen Khairi <77403713+haneen-khairi@users.noreply.github.com>
2022-07-23 12:15:25 +0300

    e

---
+++ src/ArrayState.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ArrayState = () =>{
+return(
+    <div>
+
+    </div>
+)
+
+
+}
+export default ArrayState;
---
+++ src/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './css/App.css';
+
+// my run code
+const Profile = ()=>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default Profile; 
